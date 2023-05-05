54f7a143e787269dcba60f1387069802c3551a7d
anthonynbui <anthonynbui99@gmail.com>
2023-05-05 11:32:16 -0700

    initial commit

---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 25;
+    public float attackRate = 50f;
+    float nextAttackTime = 0f;
+    public int maxHealth = 100;
+    public int currentHealth;
+    // Update is called once per frame
+
+    void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    void Start() {
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+{
+    if (Time.time >= nextAttackTime) 
+    {
+        if (Input.GetKeyDown(KeyCode.Z)) 
+        {
+            Attack();
+            nextAttackTime = Time.time + 8f / attackRate;
+            // Debug.Log("attackRate = " + attackRate);
+            // Debug.Log("Time.time = " + Time.time);
+            // Debug.Log("nextAttackTime = " + nextAttackTime);
+        }
+    }
+}   
+
+    void Attack() 
+    {
+        animator.SetTrigger("Attack");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+    
+        foreach(Collider2D enemy in hitEnemies) 
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+
+    public void TakeDamage(int damage) {
+        currentHealth = currentHealth - damage;
+
+        Debug.Log("Player took " + damage + " damage");
+    }
+
+    void OnDrawGizmosSelected() 
+    {
+        if (attackPoint == null){
+            return;
+        }
+        
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    private SpriteRenderer spriteRenderer;  
+    private AudioSource sound;
+    public float jumpRate = 90f;
+    float nextJumpTime = 0f;
+
+
+    private void Awake() {
+        body = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        // sound = GetComponent<AudioSource>();
+    }
+
+    private void Update() {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+
+        // Set player's velocity based no player input
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        if (Time.time >= nextJumpTime) {
+            if (Input.GetKey(KeyCode.Space)) 
+            {
+            body.velocity = new Vector2(body.velocity.x, speed - 5);
+            nextJumpTime = Time.time + 70f / jumpRate;
+            // sound.Play();
+            } 
+        }
+        
+
+        // Change the sprite depending on the player's movement direction
+        if (horizontalInput > 0) {
+            spriteRenderer.flipX = false; // The sprite faces right by default
+        } else if (horizontalInput < 0) {
+            spriteRenderer.flipX = true; // Flip the sprite to face left
+        }
+
+    }
+}
