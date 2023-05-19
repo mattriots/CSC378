1aadf47e0e3322b18adf4ea582679cbb1b3b51bc
Jun Liang <60671832+ordinarylif3@users.noreply.github.com>
2023-05-19 13:29:36 -0700

    polished and updated hitboxes

--- Assets/Scripts/EnemyMelee.cs
+++ Assets/Scripts/EnemyMelee.cs
@@ -17,6 +17,8 @@ public class EnemyMelee : MonoBehaviour
     [SerializeField] private LayerMask playerLayer;
     private float cooldownTimer = Mathf.Infinity;
 
+    [SerializeField] private Transform enemy;
+
     //References
     private Animator animator;
     //private Health playerHealth;
@@ -26,7 +28,6 @@ public class EnemyMelee : MonoBehaviour
     public int currentHealth;
     public GameObject player;
 
-    public Transform facePlayer;
 
     public float speed = 2f; // The speed at which the NPC moves
     public Transform leftPoint; // The left-most point the NPC will move to
@@ -41,6 +42,8 @@ public class EnemyMelee : MonoBehaviour
     public AudioClip[] deathSounds; 
     private AudioSource audioSource;
 
+    private Vector3 initScale;
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
@@ -48,6 +51,8 @@ public class EnemyMelee : MonoBehaviour
         currentHealth = maxHealth;
         spriteRenderer = GetComponent<SpriteRenderer>();
         audioSource = GetComponent<AudioSource>();
+        initScale = enemy.localScale;
+  
     }
 
     void PlayRandomHitSound()
@@ -97,8 +102,8 @@ public class EnemyMelee : MonoBehaviour
             Debug.Log("Player is in sight");
             if (cooldownTimer >= attackCooldown)
             {
-                if(!movingLeft)
-                    spriteRenderer.flipX = true;
+                // if(!movingLeft)
+                //     spriteRenderer.flipX = true;
                 cooldownTimer = 3f;
                 animator.SetTrigger("Attack");
             }
@@ -108,25 +113,29 @@ public class EnemyMelee : MonoBehaviour
             //animator.ResetTrigger("Attack");
             Walk();
             //animator.SetBool("Attack", false);
-            if(movingLeft == false){
-                spriteRenderer.flipX = false;
-            }
+
         }
 
     }
 
+
      void Walk(){
+
+        int _direction;
 
         if (movingLeft)
         {
+            
             transform.position = Vector2.MoveTowards(transform.position, leftPoint.position, speed * Time.deltaTime);
             animator.SetTrigger("Walk");
                     // Check if the NPC has reached its target point, and reverse its direction if it has
                 if (transform.position == leftPoint.position)
                 {
+                    _direction = -1;
                     movingLeft = false;
-                    spriteRenderer.flipX = false;
-                    
+                    //spriteRenderer.flipX = true;
+                    enemy.localScale = new Vector3(Mathf.Abs(initScale.x) * _direction,
+                        initScale.y, initScale.z);
                     //animator.SetTrigger("Walk"); // Trigger the MoveLeft animation clip
                 }
             
@@ -138,8 +147,11 @@ public class EnemyMelee : MonoBehaviour
 
             if (transform.position == rightPoint.position)
                 {
+                    _direction = 1;
                     movingLeft = true;
-                    spriteRenderer.flipX = true;
+                    //spriteRenderer.flipX = true;
+                    enemy.localScale = new Vector3(Mathf.Abs(initScale.x) * _direction,
+                        initScale.y, initScale.z);
                     //animator.SetTrigger("Walk"); // Trigger the MoveRight animation clip
                 }
         }
@@ -149,22 +161,29 @@ public class EnemyMelee : MonoBehaviour
 
     private bool PlayerInSight()
     {
+
         RaycastHit2D hit = 
             Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
             0, Vector2.left, 0, playerLayer);
+        
 
-        if (hit.collider != null)
+        if (hit.collider != null){
             playerHealth = hit.transform.GetComponent<PlayerCombat>();
+        
+        }
 
         return hit.collider != null;
     }
+
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
     }
+
 
     private void DamagePlayer()
     {
